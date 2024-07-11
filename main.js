c0573d2bf7c674507b60f5a7fbc5b4c16bf96eb4
S-Anbu <anbarasans750@gmail.com>
2024-07-11 14:55:21 +0530

    Added calc functionality  addad

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 import './style.css'
+
+const outputScreen = document.getElementById("output-screen");
+function display(input) {
+    outputScreen.value += input;
+}
+function calculate() {
+    try {
+        outputScreen.value = eval(outputScreen.value);
+    }
+    catch {
+        alert('invalid input')
+    }
+}
+function clear() {
+    outputScreen.value = "";
+
+}
+function del() {
+    outputScreen.value = outputScreen.value.slice(0, -1);
+}
